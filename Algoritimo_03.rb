d9b0e383b5f956dd95234bbfee54203bea27ad64
HEMILIANO OLINDA FERNANDES <hemiliano@gmail.com>
2024-07-18 16:40:31 -0300

    Update Algoritimo_03.rb
    Finalizar

--- Algoritimo_03.rb
+++ Algoritimo_03.rb
@@ -1,28 +1,17 @@
 class Tres_maiores
   def initialize(x)
     @base = x.sort.reverse
-    @primeiro = 0
-    @segundo = 0
-    @terceiro = 0
-  end
-  def confere
-
-    @base.each do |n|
-      if n > @primeiro
-        @primeiro = n
-      elsif n > @segundo
-        @segundo = n
-      elsif n > @terceiro
-        @terceiro = n
-      end
-    end
-    return "Numeros:  #{@primeiro} - #{@segundo} - #{@terceiro} - #{@base}"
+    @primeiro = @base[0]
+    @segundo = @base[1]
+    @terceiro = @base[2]
+    puts "--- São os Numeros:  [#{@primeiro} - #{@segundo} - #{@terceiro}] ------"
   end
   
 end
 
-puts "--- Na Frase ---"
-numeros = [589, 1400, 1410, 324, 501, 23, 90]
+puts "--- Os Maiores numero da basa: ---"
+numeros = [5809, 1400, 1410, 3240, 501, 23, 90]
+puts "--- Numeros: #{numeros} ---"
+puts "---------------------------------------"
 
 num = Tres_maiores.new(numeros)
-puts num.confere
